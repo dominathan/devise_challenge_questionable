d43bbd80040f58f94ffdcade08d155ba6b5ee1ca
Andrew Kennedy <akennedy@carekinesis.com>
2014-02-13 16:43:33 -0500

    don't require questions when authenticating via authentication token

--- lib/devise_challenge_questionable/hooks/challenge_questions.rb
+++ lib/devise_challenge_questionable/hooks/challenge_questions.rb
@@ -2,4 +2,7 @@ Warden::Manager.after_authentication do |user, auth, options|
   if user.respond_to?(:need_challenge_questions?)
     auth.session(options[:scope])[:need_challenge_questions] = user.need_challenge_questions?(auth.request)
   end
+  if Devise.respond_to?(:token_authentication_key) && auth.env["action_dispatch.request.parameters"].keys.include?(Devise.token_authentication_key.to_s)
+    auth.session(options[:scope])[:need_challenge_questions] = false
+  end
 end
--- lib/devise_challenge_questionable/version.rb
+++ lib/devise_challenge_questionable/version.rb
@@ -1,3 +1,3 @@
 module DeviseChallengeQuestionable
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
