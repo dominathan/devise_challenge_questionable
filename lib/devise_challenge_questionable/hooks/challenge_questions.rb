18c690d52dd3e7903de67959dcfb7de1c5c483ec
Andrew Kennedy <akennedy@carekinesis.com>
2014-02-20 00:15:10 -0500

    fix auth token bug

--- lib/devise_challenge_questionable/hooks/challenge_questions.rb
+++ lib/devise_challenge_questionable/hooks/challenge_questions.rb
@@ -1,13 +1,18 @@
 Warden::Manager.after_authentication do |user, auth, options|
-  if user.respond_to?(:login_challenge_questions?)
-    auth.session(options[:scope])[:login_challenge_questions] = user.login_challenge_questions?(auth.request)
-  end
-  
-  if user.respond_to?(:set_challenge_questions?)
-    auth.session(options[:scope])[:set_challenge_questions] = user.set_challenge_questions?(auth.request)
-  end
   
   if Devise.respond_to?(:token_authentication_key) && auth.env["action_dispatch.request.parameters"].keys.include?(Devise.token_authentication_key.to_s)
-    auth.session(options[:scope])[:need_challenge_questions] = false
+    auth.session(options[:scope])[:login_challenge_questions] = false
+    auth.session(options[:scope])[:set_challenge_questions] = false
+  else
+  
+    if user.respond_to?(:login_challenge_questions?)
+      auth.session(options[:scope])[:login_challenge_questions] = user.login_challenge_questions?(auth.request)
+    end
+  
+    if user.respond_to?(:set_challenge_questions?)
+      auth.session(options[:scope])[:set_challenge_questions] = user.set_challenge_questions?(auth.request)
+    end
+    
   end
+
 end
