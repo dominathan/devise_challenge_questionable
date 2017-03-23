95cbf1343ec7471d7ad482af6145e453bc133009
Nathan Hall <nathan.mh@gmail.com>
2017-03-23 13:40:33 -0400

    fix mailer

--- lib/devise_challenge_questionable/mailer.rb
+++ lib/devise_challenge_questionable/mailer.rb
@@ -2,7 +2,7 @@ module DeviseChallengeQuestionable
   module Mailer
     # Deliver reset challenge question instructions when is requested
     def reset_challenge_questions_instructions(record)
-      setup_mail(record, :reset_challenge_questions_instructions)
+      devise_mail(record, :reset_challenge_questions_instructions)
     end
   end
 end
